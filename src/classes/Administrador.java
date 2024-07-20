5409123f4e0094fcd29633eca26729337700cdc8
Gustavo Rutkowski <rutkowskigustavo@gmail.com>
2024-07-19 23:06:14 -0300

    Construindo o esqueleto da classe Administrador.

--- src/classes/Administrador.java
+++ src/classes/Administrador.java
@@ -2,11 +2,25 @@ package classes;
 
 public class Administrador extends Usuario {
     public Administrador(String nomeUsuario, String nomeCompleto, String email, String senha, String preferencia, String descricaoPerfil, double salario) {
-        super(nomeUsuario, nomeCompleto, email, senha, preferencia, descricaoPerfil, salario);
+        super(
+            nomeUsuario,
+            nomeCompleto,
+            email,
+            senha,
+            preferencia,
+            descricaoPerfil,
+            salario
+        );
     };
+
+    // Métodos
 
     public void banirUsuario(Usuario usuario) {
         // Implementar a lógica de banir o usuário...
+    };
+
+    public void editarDescricaoUsuario(Usuario usuario, String novaDescricao) {
+        // Implementar lógica de editar descrição do usuário...
     };
 
     public void editarAnuncio(Anuncio anuncio, String novaDescricao) {

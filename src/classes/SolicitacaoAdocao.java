bf29d3606757dc785671920f811dcc6104322d7a
Gustavo Rutkowski <rutkowskigustavo@gmail.com>
2024-07-19 23:02:35 -0300

    Construindo o esqueleto da classe SolicitacaoAdocao.

--- src/classes/SolicitacaoAdocao.java
+++ src/classes/SolicitacaoAdocao.java
@@ -1,9 +1,35 @@
 package classes;
 
-// Ela possui o status da solicitação (pendente, negada ou aceita)
-
-// Precisa de um anúncio pra existir
-// Precisa de um usuário pra existir
 public class SolicitacaoAdocao {
-    
-}
+    private String status;
+    private Anuncio anuncio;
+    private Usuario autor;
+
+    public SolicitacaoAdocao(String status, Anuncio anuncio, Usuario autor) {
+        this.status = status;
+        this.anuncio = anuncio;
+        this.autor = autor;
+    };
+
+    // Getters
+
+    public String getStatus() {
+        return this.status;
+    };
+
+    public Anuncio getAnuncio() {
+        return this.anuncio;
+    };
+
+    public Usuario getAutor() {
+        return this.autor;
+    };
+
+    // Métodos
+
+    public void imprimirDados() {
+        System.out.println("status: " + this.getStatus());
+        System.out.println("anuncio: " + this.getAnuncio());
+        System.out.println("autor: " + this.getAutor());
+    };
+};
